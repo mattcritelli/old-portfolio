c24cc082e056d021034106eceffb9781f1deec17
Matt C <matthew.critelli@gmail.com>
2018-02-15 16:24:23 -0700

    Add About section and slideshow to main section. Add a couple small projects. Add download resume button to contact section.

---
+++ script.js
@@ -0,0 +1,10 @@
+$('#slideshow > div:gt(0)').hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1200)
+    .next()
+    .fadeIn(1200)
+    .end()
+    .appendTo('#slideshow')
+}, 2000);
